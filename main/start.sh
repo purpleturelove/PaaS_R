0a842ec8a65c65ee7c1858dbcacfbec2eac530a3
purpleturelove <87450149+purpleturelove@users.noreply.github.com>
2022-10-05 01:01:11 -0800

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,5 +1,5 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export UUID=${UUID-dfc81986-30a7-4337-b29c-234d6f3304bf}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
